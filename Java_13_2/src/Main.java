1fd701f344d4cffdab0df46ea1df302f1bc714ec
soeuns <soensuckun@naver.com>
2023-07-06 21:34:52 +0900

    파일 입출력 실습

--- Java_13_2/src/Main.java
+++ Java_13_2/src/Main.java
@@ -7,17 +7,47 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //      1. 파일 쓰기
 //      FileWriter
-        
+        FileWriter fw = new FileWriter("./Java_13_2/memo.txt"); //현재 폴더에 memo라는 파일 만듦
+        String memo = "헤드라인 \n";
+        fw.write(memo);
+
+        memo = "1월 28일 소은 생일 \n";
+        fw.write(memo);
+
+        fw.close();
 
 //      PrintWriter
-        
+        PrintWriter pw = new PrintWriter("./Java_13_2/memo1.txt");
+        memo = "헤드빙빙";
+        pw.println(memo); //별도로  \n안해줘도 됨
+
+        memo = "7/6 배고픔";
+        pw.println(memo);
+
+        pw.close();
 
 //      파일 이어 쓰기
+        FileWriter fw2 = new FileWriter("./Java_13_2/memo.txt",true);
+        memo="코딱지 \n";
+        fw2.write(memo);
+        fw2.close();
 
+        PrintWriter pw2 = new PrintWriter(new FileWriter("./Java_13_2/memo1.txt",true));
+        memo="차돌곱도리탕 시킴";
+        pw2.println(memo);
 
+        pw2.close();
 
 //      2. 파일 입력
+        BufferedReader br = new BufferedReader(new FileReader("./Java_13_2/memo1.txt"));
 
-
+        while(true){
+            String line = br.readLine(); //파일 한 줄 씩 읽어들임
+            if(line == null) //읽을 데이터 없으면
+                break;
+            else
+                System.out.println(line);
+        }
+        br.close();
     }
 }
--- Java_13_2/src/Practice.java
+++ Java_13_2/src/Practice.java
@@ -1,10 +1,7 @@
 // Practice
 // JamesArthurGosling.txt 파일을 읽은 후 원하는 단어 변경하여 새로 저장해보자.
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Practice {
@@ -13,10 +10,29 @@ public class Practice {
         String outputFile = "./JamesArthurGosling_edit.txt";
 
         // 찾을 단어 / 변경 단어 입력 받기
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("찾을 단어 :");
+        String findWord = sc.nextLine();
+        System.out.println("바꿀단어 :");
+        String newWord = sc.nextLine();
 
         // 파일 읽기, 변경 및 저장
+            //파일 읽기
+        BufferedReader br = new BufferedReader(new FileReader("./Java_13_2/JamesArthurGosling.txt"));
 
+        FileWriter fw = new FileWriter("./Java_13_2/JamesArthurGosling_edit.txt");
+//      PrintWriter pw = new PrintWriter("./Java_13_2/JamesArthurGosling_edit.txt");
+        while(true){
+            String line = br.readLine();
 
+            if(line == null) break;
+
+            line = line.replace(findWord, newWord);
+            fw.write(line + "\n");
+            // pw.println(line);
+        }
+        br.close();
+        fw.close();
+        //pw.close();
     }
 }
