d146deb41c7f47dee1c57f9a79ec82667f1914a9
soeuns <soensuckun@naver.com>
2023-07-06 23:53:14 +0900

    람다식 실습

--- Java_16/src/Main.java
+++ Java_16/src/Main.java
@@ -3,7 +3,7 @@
 interface ComputeTool {
     public abstract int compute(int x, int y);
 
-//    public abstract int compute2(int x, int y);
+ //   public abstract int compute2(int x, int y);
 }
 
 
@@ -12,8 +12,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        ComputeTool cTool1 = new ComputeTool(){ //객체를 만들 떄 익명클래스로 만듦
+            @Override
+            public int compute(int x, int y) {
+                return x+y;
+            }
+//            @Override
+//            public int compute2(int x, int y) {
+//                return x-y;
+//            }
+        };
+        System.out.println(cTool1.compute(3,4));
         // 람다식
+        ComputeTool cTool2 = (x,y) -> { return x+y; };
+        System.out.println(cTool2.compute(5,7));
 
     }
 }
--- Java_16/src/Practice.java
+++ Java_16/src/Practice.java
@@ -18,6 +18,7 @@ public class Practice {
 
 
         // 람다식으로 작성
-
+        CompareTool cTool2 = (x,y)-> { return x > y ? x : y ;};
+        System.out.println(cTool2.getMaxNum(50,60));
     }
 }
