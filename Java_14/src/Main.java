34ed5abf8ce80c7474222e4e13db7a2b67c77f68
soeuns <soensuckun@naver.com>
2023-07-06 22:36:30 +0900

    예외처리 실습

--- Java_14/src/Main.java
+++ Java_14/src/Main.java
@@ -1,10 +1,11 @@
 // Java 프로그래밍 - 예외 처리
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
+class NotTenException extends RuntimeException{}
 public class Main {
 
     public static boolean checkTen(int ten) {
@@ -16,14 +17,21 @@ public class Main {
     }
 
     public static boolean checkTenWithException(int ten) {
-
-
+        try {
+            if (ten != 10) {
+                throw new NotTenException();
+            }
+        }catch (NotTenException e){
+            System.out.println("e = " + e);
+            return false;
+        }
         return true;
     }
 
-    public static boolean checkTenWithThrows(int ten) {
-
-
+    public static boolean checkTenWithThrows (int ten) throws NotTenException{
+        if(ten != 10){
+            throw new NotTenException();
+        }
         return true;
     }
 
@@ -33,18 +41,33 @@ public class Main {
 //      1-1. 0으로 나누기
         System.out.println("== 0으로 나누기 ==");
 //      int a = 5 / 0;
-
+        try{
+            int a = 5 / 0;
+        }catch(ArithmeticException e){
+            System.out.println(e);
+        } finally{
+            System.out.println("1-1 연습 종료");
+        }
 
 //      1-2. 배열 인덱스 초과
         System.out.println("== 배열 인덱스 초과 ==");
         int[] b = new int[4];
 //        b[4] = 1;
-
+        try{
+            b[4] = 1;
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("배열 인덱스 오류");
+            System.out.println("e = " + e);
+        }
         
 //      1-3. 없는 파일 열기
         System.out.println("== 없는 파일 열기 ==");
 //        BufferedReader br = new BufferedReader(new FileReader("abc.txt"));
-
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("abc.txt"));
+        }catch( FileNotFoundException e){
+            System.out.println("e = " + e);
+        }
 
 //      2. throw, throws
         System.out.println("== checkTen ==");
@@ -53,11 +76,16 @@ public class Main {
 
 
         System.out.println("== checkTenWithException ==");
-
+        checkResult = Main.checkTenWithException(7);
+        System.out.println(checkResult);
 
         System.out.println("== checkTenWithThrows ==");
-
-
+        try{
+            checkResult = checkTenWithThrows(5);
+        }catch (NotTenException e){
+            System.out.println("e = " + e);
+        }
+        System.out.println("checkResult = "+checkResult);
     }
 
 }
