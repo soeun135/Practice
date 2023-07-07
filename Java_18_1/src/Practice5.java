0804af678b8f26b95ee40b6c7140f7d5409de9be
soeuns <soensuckun@naver.com>
2023-07-08 00:40:40 +0900

    연습문제 5 실습

--- Java_18_1/src/Practice5.java
+++ Java_18_1/src/Practice5.java
@@ -1,7 +1,32 @@
 
 public class Practice5 {
     public static int solution(int[] height) {
-        return 0;
+        //내코드
+//        int max = 0;
+//        for (int i = 0; i < height.length; i++) {
+//            for (int j = i+1; j < height.length; j++) {
+//                int width= j-i;
+//                int hei= height[i]>height[j] ? height[j]:height[i];
+//                if(max < hei*width)
+//                    max = hei*width;
+//            }
+//        }
+//        return max;
+        int left = 0;
+        int right = height.length-1;
+        int max = 0;
+
+        while(left <right){
+            int x = right-left;
+            int y = height[left] < height[right] ? height[left] : height[right];
+            int curArea = x * y;
+            max= max < curArea?curArea:max;
+
+            if(height[left] < height[right])
+                left++;
+            else right--;
+        }
+        return max;
     }
 
     public static void main(String[] args) {
