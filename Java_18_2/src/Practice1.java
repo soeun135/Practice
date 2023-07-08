967c38df9a79aca61057317dd716bcac3af6203b
soeuns <soensuckun@naver.com>
2023-07-08 20:41:59 +0900

    18_2 연습문제 1 로마문자 정수로 바꾸기완료

--- Java_18_2/src/Practice1.java
+++ Java_18_2/src/Practice1.java
@@ -1,7 +1,36 @@
+import java.util.HashMap;
 
 public class Practice1 {
     public static void solution(String s) {
+        //char []str = {'I','V','X','L','C','D','M'};
+        int result = 0;
+        String str = "IVXLCDM";
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);map.put('V',5);map.put('X',10);map.put('L',50);map.put('C',100);map.put('D',500);map.put('M',1000);
 
+        int sum = 0;
+        boolean twin = false;
+        int i = 1;
+        while(true){
+            if(i >= s.length()) break;
+
+            char c = s.charAt(i);
+            char c_ = s.charAt(i-1);
+            if(str.indexOf(c_) < str.indexOf(c)){
+                sum += map.get(c)-map.get(c_);
+                i+=2;
+                twin = true;
+            }
+            else{
+                sum += map.get(c_);
+                twin = false;
+                i++;
+            }
+        }
+        if(!twin){
+            sum += map.get(s.charAt(s.length()-1));
+        }
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
