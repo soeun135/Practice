1e90596ccff006f6ab88af1b4efdec4c2acd2b41
soeuns <soensuckun@naver.com>
2023-07-10 14:26:26 +0900

    Part02 실습코드 업로드 02

--- Part02/Math_02/src/Main.java
+++ Part02/Math_02/src/Main.java
@@ -13,11 +13,11 @@ public class Main {
         set1.add(1);
         set1.add(1);
         set1.add(1);
-        System.out.println("set1 = " + set1);
+        System.out.println("set1 = " + set1); //set은 데이터 중복 안 돼서 1만 출력
         set1.add(2);
         set1.add(3);
         System.out.println("set1 = " + set1);
-        set1.remove(1);
+        set1.remove(1);//remove(값)
         System.out.println("set1 = " + set1);
         System.out.println(set1.size());
         System.out.println(set1.contains(2));
@@ -27,13 +27,18 @@ public class Main {
         System.out.println("== 집합 연산 ==");
 
 //      2-1. 교집합
-
+        HashSet a = new HashSet(Arrays.asList(1,2,3,4,5));
+        HashSet b = new HashSet(Arrays.asList(2,4,6,8,10));
+//        a.retainAll(b); //교집합 계산
+//        System.out.println("교집합 : "+a);
 
 //      2-2. 합집합
-
+//        a.addAll(b);
+//        System.out.println("합집합 : "+a);
 
 //      2-3. 차집합
-
+        a.removeAll(b);
+        System.out.println("차집합 : "+a);
 
     }
 
