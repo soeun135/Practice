6d1cb089831aad971e4d446119e0da833e840a80
soeuns <soensuckun@naver.com>
2023-07-15 02:08:00 +0900

    stack 문제풀이 완료

--- Part02_/Chapter02/LinearDS_07/src/Practice3.java
+++ Part02_/Chapter02/LinearDS_07/src/Practice3.java
@@ -12,19 +12,75 @@
 import java.util.Stack;
 
 public class Practice3 {
-//    public static double calculate(String string) {
+    public static double calculate(String string) {
+        //강사님코드
+        Stack <Double>stack = new Stack();
+        for(String s : string.split(" ")) {//공백기준으로 문자열을 쪼갬
+            if (s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                stack.push(1 / stack.pop() * stack.pop());
+            } else {
+                stack.push(Double.parseDouble(s));
+            }
+        }
+            return stack.pop();
+            //내코드
+//        Stack <String>stack = new Stack();
+//        int data1 = 0;
+//        int data2 = 0;
+//        double result = 0;
+//        for (int i = 0; i < string.length(); i++) {
+//            char c = string.charAt(i);
 //
-//    }
+//            if('0' <= c && c <= '9'){
+//                stack.push(Character.toString(c));
+//            }
+//            else if(c==' '){
+//                continue;
+//            }
+//            else{
+//                int cnt = 0;
+//                char op = c;
+//                data1 = Integer.parseInt(stack.pop());
+//                data2 = Integer.parseInt(stack.pop());
+//                switch(op){
+//                    case '+':
+//                        cnt = data1+data2;
+//                        break;
+//                    case '*':
+//                        cnt =(data1*data2);
+//                        break;
+//                    case '/':
+//                        cnt =(data2/data1);
+//                        break;
+//                    case '-':
+//                        cnt =(data2-data1);
+//                        break;
+//                }
+//                stack.push(Integer.toString(cnt));
+//            }
+//        }
+//        if(!stack.isEmpty()){
+//            result = Double.parseDouble(stack.pop());
+//        }
+//        return result;
+    }
+
 
     public static void main(String[] args) {
         // Test code
-//        System.out.println(calculate("2 2 +"));    // 4
-//        System.out.println(calculate("2 2 -"));    // 0
-//        System.out.println(calculate("2 2 *"));    // 4
-//        System.out.println(calculate("2 2 /"));    // 1
-//
-//        System.out.println(calculate("1 1 + 2 * 3 * 2 / 5 -"));    // 1
-//        System.out.println(calculate("5 2 * 3 - 8 * 4 /"));        // 14
+        System.out.println(calculate("2 2 +"));    // 4
+        System.out.println(calculate("2 2 -"));    // 0
+        System.out.println(calculate("2 2 *"));    // 4
+        System.out.println(calculate("2 2 /"));    // 1
+
+        System.out.println(calculate("1 1 + 2 * 3 * 2 / 5 -"));    // 1
+        System.out.println(calculate("5 2 * 3 - 8 * 4 /"));        // 14
 
     }
 }
