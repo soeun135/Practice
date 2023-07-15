ec0050b06538c4f7c1255f4e62a0a3a1d75bf740
soeuns <soensuckun@naver.com>
2023-07-16 03:35:49 +0900

    Deque 실습 완료

--- Part02_/Chapter02/LinearDS_10/src/Practice2.java
+++ Part02_/Chapter02/LinearDS_10/src/Practice2.java
@@ -2,30 +2,69 @@
 // 배열을 이용한 기본 데크 직접 구현
 
 class MyDeque2 {
-
+    int arr[];
+    int front = 0;
+    int rear = 0;
     MyDeque2(int size) {
+        this.arr = new int[size + 1];
     }
 
-//    public boolean isEmpty() {
-//    }
-//
-//    public boolean isFull() {
-//    }
-//
-//    public void addFirst(int data) {
-//    }
-//
-//    public void addLast(int data) {
-//    }
-//
-//    public Integer removeFirst() {
-//    }
-//
-//    public Integer removeLast() {
-//    }
-//
-//    public void printDeque() {
-//    }
+    public boolean isEmpty() {
+        return this.front == this.rear;
+    }
+
+    public boolean isFull() {
+        return (this.rear + 1) % this.arr.length == this.front;
+    }
+
+    public void addFirst(int data) {
+        if(this.isFull()){
+            System.out.println("Dequq is Full");
+            return;
+        }
+        //front가 가리키고있는 부분엔 데이터 없다고 생각
+        //원형 큐 처럼
+        this.arr[this.front] = data;
+        this.front = (this.arr.length + (this.front - 1)) % this.arr.length;
+    }
+
+    public void addLast(int data) {
+        if(this.isFull()){
+            System.out.println("Dequq is Full");
+            return;
+        }
+        this.rear = (this.rear + 1) % arr.length;
+        this.arr[this.rear] = data;
+    }
+
+    public Integer removeFirst() {
+        if (isEmpty()){
+            System.out.println("Deque is Empty");
+            return null;
+        }
+
+        this.front = (this.front + 1) % this.arr.length;
+        return this.arr[this.front];
+    }
+
+    public Integer removeLast() {
+        if (isEmpty()){
+            System.out.println("Deque is Empty");
+            return null;
+        }
+        int data = this.arr[this.rear];
+        this.rear = (this.arr.length + (this.rear - 1)) % this.arr.length;
+        return data;
+    }
+
+    public void printDeque() {
+        int start = (this.front + 1) % this.arr.length;
+        int end = (this.rear + 1) % this.arr.length;
+        for (int i = start; i != end ; i = (i + 1) % this.arr.length) {
+            System.out.print(this.arr[i]+" ");
+        }
+        System.out.println();
+    }
 
 }
 
@@ -34,28 +73,28 @@ public class Practice2 {
         // Test code
         MyDeque2 myDeque = new MyDeque2(5);
         // Front 부분 입력
-//        myDeque.addFirst(1);
-//        myDeque.addFirst(2);
-//        myDeque.addFirst(3);
-//        myDeque.printDeque();   // 3 2 1
-//
-//        // Rear 부분 입력
-//        myDeque.addLast(10);
-//        myDeque.addLast(20);
-//        myDeque.addLast(30);    // Deque is full!
-//        myDeque.printDeque();        // 3 2 1 10 20
-//
-//        // Front 부분 출력
-//        System.out.println(myDeque.removeFirst());  // 3
-//        myDeque.printDeque();   // 2 1 10 20
-//
-//        // Rear 부분 출력
-//        System.out.println(myDeque.removeLast());   // 20
-//        myDeque.printDeque();    // 2 1 10
-//
-//        System.out.println(myDeque.removeLast());   // 10
-//        System.out.println(myDeque.removeLast());   // 1
-//        System.out.println(myDeque.removeLast());   // 2
-//        System.out.println(myDeque.removeLast());   // Deque is empty!
+        myDeque.addFirst(1);
+        myDeque.addFirst(2);
+        myDeque.addFirst(3);
+        myDeque.printDeque();   // 3 2 1
+
+        // Rear 부분 입력
+        myDeque.addLast(10);
+        myDeque.addLast(20);
+        myDeque.addLast(30);    // Deque is full!
+        myDeque.printDeque();        // 3 2 1 10 20
+
+        // Front 부분 출력
+        System.out.println(myDeque.removeFirst());  // 3
+        myDeque.printDeque();   // 2 1 10 20
+
+        // Rear 부분 출력
+        System.out.println(myDeque.removeLast());   // 20
+        myDeque.printDeque();    // 2 1 10
+
+        System.out.println(myDeque.removeLast());   // 10
+        System.out.println(myDeque.removeLast());   // 1
+        System.out.println(myDeque.removeLast());   // 2
+        System.out.println(myDeque.removeLast());   // Deque is empty!
     }
 }
