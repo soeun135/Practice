cc8163f6094ed994779d1e58797ec30e07284bd9
soeuns <soensuckun@naver.com>
2023-07-25 08:27:01 +0900

    이진 탐색 트리 1

--- Part02_/Chapter03/NonLinearDS_03_1/src/Main.java
+++ Part02_/Chapter03/NonLinearDS_03_1/src/Main.java
@@ -1,5 +1,7 @@
 // 비선형 자료구조 - 이진 탐색 트리
 
+import jdk.nashorn.internal.ir.JoinPredecessor;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -23,11 +25,103 @@ class BinarySearchTree {
     }
 
     public void addNode(int key) {
+        if(this.head == null){
+            this.head = new Node(key, null, null);
+        } else{
+            Node cur = this.head;
+            while(true){
+                Node pre = cur;
+
+                if(key < cur.key){
+                    cur = cur.left;
+
+                    if(cur == null){
+                        pre.left = new Node(key, null, null);
+                        break;
+                    }
+                } else{
+                    cur = cur.right;
+
+                    if(cur == null){
+                        pre.right = new Node(key, null, null);
+                        break;
+                    }
+                }
+            }
+        }
 
     }
 
     public void removeNode(int key) {
+        Node parent = null;
+        Node successor = null; //지울려는 노드의 자식 노드 중 후계자 찾을 변수
+        Node predecessor = null; //지울려는 노드의 부모노드를 가리킬 변수
+        Node child = null; //sucessor의 자식이 있는지 확인할 변수
 
+        Node cur = this.head;
+        while(cur != null){
+            if(cur.key == key){ //cur에는 삭제하려는 노드 값
+                break;
+            }
+            parent = cur; //삭제하려고 하는 노드의 부모노드 값을 가지고 있음
+            if (key < cur.key){
+                cur = cur.left;
+            } else{
+                cur = cur.right;
+            }
+        }
+        if (cur == null){
+            System.out.println("key에 해당하는 노드가 없습니다.");
+            return;
+        }
+        if(cur.left == null && cur.right == null){ //삭제하려는 노드가 leaf노드
+            if (parent == null){
+                this.head = null;
+            } else {
+                if (parent.left == cur){
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+        } else if((cur.left != null && cur.right == null) || (cur.left == null && cur.right != null)){ //자식이 하나만 있으면
+            if (cur.left != null){
+                child = cur.left;
+            } else{
+                child = cur.right;
+            }
+            if (parent == null){ //루트노드에 자식노드가 하나만 있는 형태
+                this.head = child;
+            } else {
+                if (parent.left == cur) {
+                    parent.left = child;
+                } else {
+                    parent.right = child;
+                }
+            }
+        } else { //자식 노드가 둘인 경우
+            predecessor = cur;
+            successor = cur.left; //삭제할 노드의 왼쪽 서브트리에서 가장 큰 값을 찾을 거임.
+
+            while(successor.right != null){
+                predecessor = successor;
+                successor = successor.right; //후계자 선택
+            }
+            predecessor.right = successor.left; //successor의 right는 없음 있었으면 위에서 successor이 됐을 거임
+            //아무튼 얘는 위로 올라갈 거니까 얘의 왼쪽 자식노드를 부모노드의 오른 쪽에 붙여줌
+            successor.left = cur.left;
+            successor.right = cur.right;
+
+            if (parent == null){ //루트노드이면ㅁ서 자식노드가 두 개인 경우
+                this.head = successor;
+            } else{
+                if (parent.left == cur){
+                    parent.left = successor;
+                } else{
+                    parent.right = successor;
+                }
+            }
+        }
     }
 
     public void levelOrder(Node node) {
@@ -63,6 +157,7 @@ public class Main {
         bst.addNode(25);
         bst.addNode(13);
         bst.addNode(35);
+        bst.addNode(33);
         bst.addNode(27);
         bst.addNode(40);
         bst.levelOrder(bst.head);
--- Part02_/Chapter03/NonLinearDS_03_1/src/Practice1.java
+++ Part02_/Chapter03/NonLinearDS_03_1/src/Practice1.java
@@ -12,13 +12,44 @@ class BinarySearchTree2 {
     }
 
     public Node addNodeRecursive(Node cur, int key) {
-
-        return null;
+        if (cur == null){
+            return new Node(key, null, null);
+        }
+        if (key < cur.key) {
+            cur.left = addNodeRecursive(cur.left, key);
+        } else{
+            cur.right = addNodeRecursive(cur.right, key);
+        }
+        return cur;
     }
 
     public Node removeNodeRecursive(Node cur, int key) {
+        if(cur == null){
+            return null;
+        }
+        if (key < cur.key){
+            cur.left = removeNodeRecursive(cur.left, key);
+        } else if (key > cur.key){
+            cur.right = removeNodeRecursive(cur.right, key);
+        } else {//삭제하려고 하는 노드를 찾은 케이스
+            //자식노드가 하나이거나 없는 경우
+            if (cur.left == null){
+                return cur.right; //삭제하려는 노드의 왼쪽 자식이 없는 경우 오른쪽 자식노드 리턴
+            } else if (cur.right == null){
+                return cur.left;
+            } else{ //자식노드가 둘인 경우
+                Node predecessor = cur;
+                Node successor = cur.left;
 
-        return null;
+                while(successor.right != null){
+                    predecessor = successor;
+                    successor = successor.right;
+                }
+                predecessor.right = successor.left;
+                cur.key = successor.key; //오 링크 연결을 다시 해주는게 아니라 해당 노드 자리에 데이터만 바꿔 줌 !!
+            }
+        }
+        return cur;
     }
 
     public void levelOrder(Node node) {
