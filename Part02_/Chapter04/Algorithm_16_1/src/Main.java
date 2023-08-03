9c781abbcda9e279490f930bc69269e60c020413
soeuns <soensuckun@naver.com>
2023-08-04 06:44:32 +0900

    최단경로 1 - 다익스트라 공부 완료

--- Part02_/Chapter04/Algorithm_16_1/src/Main.java
+++ Part02_/Chapter04/Algorithm_16_1/src/Main.java
@@ -1,11 +1,64 @@
 // 알고리즘 - 최단 경로 알고리즘
 // 다익스트라 기본 구현
 
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void dijkstra(int v, int[][] data, int start) {
+    static class Node {
+        int to; //해당 노드에서
+        int weight; //어떤 노드로 weight만큼의 비용이 든다.
 
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+    public static void dijkstra(int v, int[][] data, int start) {
+        ArrayList <ArrayList<Node>> graph = new ArrayList<>();
+        for (int i = 0; i < v + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < data.length; i++) {
+            graph.get(data[i][0]).add(new Node(data[i][1], data[i][2]));
+        }
+
+        int[] dist = new int[v + 1];
+
+        for (int i = 1; i < v + 1; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+        dist[start] = 0;
+
+        boolean[] visited = new boolean[v + 1];
+
+        for (int i = 0; i < v; i++) {
+            int minDist = Integer.MAX_VALUE;
+            int curIdx = 0;
+            for (int j = 1; j < v + 1; j++) {
+                if (!visited[j] && dist[j] < minDist) {
+                    minDist = dist[j];
+                    curIdx = j;
+                }
+            }
+            visited[curIdx] = true;
+
+            for (int j = 0; j < graph.get(curIdx).size(); j++) {
+                Node adjNode = graph.get(curIdx).get(j);
+                if (dist[adjNode.to] > dist[curIdx] + adjNode.weight) {
+                    dist[adjNode.to] = dist[curIdx] + adjNode.weight;
+                }
+            }
+        }
+
+        for (int i = 1; i < v + 1; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                System.out.print("INF ");
+            } else {
+                System.out.print(dist[i] + " ");
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
--- Part02_/Chapter04/Algorithm_16_1/src/Main2.java
+++ Part02_/Chapter04/Algorithm_16_1/src/Main2.java
@@ -1,10 +1,64 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 // 다익스트라 우선순위 큐 사용
-
-
 public class Main2 {
+    static class Node {
+        int to; //해당 노드에서
+        int weight; //어떤 노드로 weight만큼의 비용이 든다.
+
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
 
     public static void dijkstra(int v, int[][] data, int start) {
+        ArrayList <ArrayList<Node>> graph = new ArrayList<>();
 
+        for (int i = 0; i < v + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < data.length ; i++) {
+            graph.get(data[i][0]).add(new Node(data[i][1], data[i][2]));
+        }
+        int dist[] = new int[v + 1];
+
+        for (int i = 1; i < v + 1; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+        dist[start] = 0;
+
+        PriorityQueue <Node> pq = new PriorityQueue<>((x, y) -> x.weight - y.weight);//weight 기준 minheap으로 pq구성
+        pq.offer(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+
+            if (dist[curNode.to] < curNode.weight) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(curNode.to).size() ; i++) {
+                Node adjNode = graph.get(curNode.to).get(i);
+
+                if (dist[adjNode.to] > curNode.weight + adjNode.weight) {
+                    dist[adjNode.to] = curNode.weight + adjNode.weight;
+                    pq.offer(new Node(adjNode.to, dist[adjNode.to]));
+                }
+            }
+        }
+
+
+        //출력부분
+        for (int i = 1; i < v + 1; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                System.out.print("INF ");
+            } else {
+                System.out.print(dist[i] + " ");
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
