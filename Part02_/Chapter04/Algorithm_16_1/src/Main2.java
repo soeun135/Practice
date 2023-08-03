4513ff45282876a434ecb41843ba1621536879f7
soeuns <soensuckun@naver.com>
2023-08-04 06:45:52 +0900

    최단경로 1 - 다익스트라 공부 완료

--- Part02_/Chapter04/Algorithm_16_1/src/Main2.java
+++ Part02_/Chapter04/Algorithm_16_1/src/Main2.java
@@ -11,55 +11,56 @@ public class Main2 {
             this.to = to;
             this.weight = weight;
         }
+    }
 
-    public static void dijkstra(int v, int[][] data, int start) {
-        ArrayList <ArrayList<Node>> graph = new ArrayList<>();
+        public static void dijkstra(int v, int[][] data, int start) {
+            ArrayList<ArrayList<Node>> graph = new ArrayList<>();
 
-        for (int i = 0; i < v + 1; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < data.length ; i++) {
-            graph.get(data[i][0]).add(new Node(data[i][1], data[i][2]));
-        }
-        int dist[] = new int[v + 1];
-
-        for (int i = 1; i < v + 1; i++) {
-            dist[i] = Integer.MAX_VALUE;
-        }
-        dist[start] = 0;
-
-        PriorityQueue <Node> pq = new PriorityQueue<>((x, y) -> x.weight - y.weight);//weight 기준 minheap으로 pq구성
-        pq.offer(new Node(start, 0));
-
-        while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-
-            if (dist[curNode.to] < curNode.weight) {
-                continue;
+            for (int i = 0; i < v + 1; i++) {
+                graph.add(new ArrayList<>());
             }
 
-            for (int i = 0; i < graph.get(curNode.to).size() ; i++) {
-                Node adjNode = graph.get(curNode.to).get(i);
+            for (int i = 0; i < data.length; i++) {
+                graph.get(data[i][0]).add(new Node(data[i][1], data[i][2]));
+            }
+            int dist[] = new int[v + 1];
 
-                if (dist[adjNode.to] > curNode.weight + adjNode.weight) {
-                    dist[adjNode.to] = curNode.weight + adjNode.weight;
-                    pq.offer(new Node(adjNode.to, dist[adjNode.to]));
+            for (int i = 1; i < v + 1; i++) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+            dist[start] = 0;
+
+            PriorityQueue<Node> pq = new PriorityQueue<>((x, y) -> x.weight - y.weight);//weight 기준 minheap으로 pq구성
+            pq.offer(new Node(start, 0));
+
+            while (!pq.isEmpty()) {
+                Node curNode = pq.poll();
+
+                if (dist[curNode.to] < curNode.weight) {
+                    continue;
+                }
+
+                for (int i = 0; i < graph.get(curNode.to).size(); i++) {
+                    Node adjNode = graph.get(curNode.to).get(i);
+
+                    if (dist[adjNode.to] > curNode.weight + adjNode.weight) {
+                        dist[adjNode.to] = curNode.weight + adjNode.weight;
+                        pq.offer(new Node(adjNode.to, dist[adjNode.to]));
+                    }
                 }
             }
-        }
 
 
-        //출력부분
-        for (int i = 1; i < v + 1; i++) {
-            if (dist[i] == Integer.MAX_VALUE) {
-                System.out.print("INF ");
-            } else {
-                System.out.print(dist[i] + " ");
+            //출력부분
+            for (int i = 1; i < v + 1; i++) {
+                if (dist[i] == Integer.MAX_VALUE) {
+                    System.out.print("INF ");
+                } else {
+                    System.out.print(dist[i] + " ");
+                }
             }
+            System.out.println();
         }
-        System.out.println();
-    }
 
     public static void main(String[] args) {
         // Test code
